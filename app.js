0777faca66e960321a6d8021a5d96a264c1add4c
melodoc <liubov_rz@yahoo.com>
2022-09-11 17:08:01 +0400

    feat. Set up mongoose

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const URI = 'mongodb://localhost:27017/mestodb';
+
+mongoose.connect(URI, {
+}, (err) => {
+  if (err) throw err.message;
+  console.log(`Connected to ${URI}`);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
