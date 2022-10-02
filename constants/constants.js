a0cecabd387f760379c756d97ea2ad7ee71db9e2
melodoc <liubov_rz@yahoo.com>
2022-10-03 01:05:37 +0400

    feat. Validate data at the schema level

--- constants/constants.js
+++ constants/constants.js
@@ -4,8 +4,10 @@ const UPDATE_PARAMS = {
 };
 
 const SECRET_KEY = 'some-secret-key';
+const URL_REG_EXP = /^(https?:\/\/)(www\.)?([a-z1-9-]{2,}\.)+[a-z]{2,}\/?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*/i;
 
 module.exports = {
   UPDATE_PARAMS,
   SECRET_KEY,
+  URL_REG_EXP,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,33 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const {
   getUsers, getUsersById, updateProfile, updateAvatar, getCurrentUser,
 } = require('../controllers/users');
+const { URL_REG_EXP } = require('../constants/constants');
 
 router.get('/', getUsers);
-router.get('/:userId', getUsersById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24).required(),
+  }),
+  headers: Joi.object().keys({
+  }).unknown(true),
+}), getUsersById);
 router.get('/me', getCurrentUser);
-router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+  headers: Joi.object().keys({
+  }).unknown(true),
+}), updateProfile);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri().regex(URL_REG_EXP).required(),
+  }),
+  headers: Joi.object().keys({
+  }).unknown(true),
+}), updateAvatar);
 
 module.exports = router;
